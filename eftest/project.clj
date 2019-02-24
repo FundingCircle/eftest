8db0a3ad57e9d58634e32ff382a2e9fee2cc2902
Avi Flax <avi.flax@fundingcircle.com>
2019-02-23 19:27:06 -0500

    Move dependency specs to deps.edn...
    ...and use lein-tools-deps so that Leiningen will still work.
    
    The main value prop here, as I see it, is that projects using tools.deps
    can include eftest via its git repo via SHA, which means that they could
    easily use a version of eftest that exists in the repo but has not been
    published to Clojars in a release artifact. And that’d apply to _any_
    version in the repo — even in a feature branch. It’d also be easy for
    projects to use forks of eftest — they’d simply change the git URL in
    their deps.edn to point to the fork repo.
    
    For example, #63 was merged to master a week ago, and I’m eager to use
    it in my project, but I can’t because a new release with the change
    hasn’t yet been published to Clojars. Once _this_ change is merged to
    master, I’ll be able to use the very latest code in master without
    waiting for it to be published.
    
    The motivation to use lein-tools-deps is to preserve the existing
    workflows for running tests, publishing releases, etc, without
    duplicating the dependency specifications.

--- eftest/project.clj
+++ eftest/project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/weavejester/eftest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [progrock "0.1.2"]
-                 [io.aviso/pretty "0.1.34"]
-                 [mvxcvi/puget "1.0.3"]])
+  :plugins [[lein-tools-deps "0.4.3"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:project]})
